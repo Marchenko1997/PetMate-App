e3bf80c6431a35a6a4ea76258e6111a023645451
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-10-21 11:14:53 +0200

    starting of login page

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,11 @@
+import { HomePageSection } from "../../components/HomePageSection/HomePageSection";
+import { DesktopBox } from "../../components/Common/DesktopBox/DesktopBox";
+
+
+const Login = () => {
+  return (
+    <HomePageSection></HomePageSection>
+  )
+}
+
+export default Login
