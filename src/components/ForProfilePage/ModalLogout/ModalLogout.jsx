c69d939d2037cbac87e35600c69ad26ba93f76ae
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-02 20:27:46 +0100

    creating ModalLogout

---
+++ src/components/ForProfilePage/ModalLogout/ModalLogout.jsx
@@ -0,0 +1,66 @@
+// @ts-nocheck
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { logOut } from "../../../redux/auth/operations";
+import ModalGeneral from "../../ModalGeneral/ModalGeneral";
+import imgModal from "../../../assets/images/modals/formodal.webp";
+import {
+  BtnsBox,
+  CancelBtn,
+  ImgContainer,
+  LogoutConfirmBtn,
+} from "./ModalLogout.styled";
+
+const ModalLogout = ({ setShowLogout, showLogout }) => {
+  const dispatch = useDispatch();
+  const handleCloseModal = () => {
+    setShowLogout(false);
+  };
+
+  const handleLogout = () => {
+    dispatch(logOut());
+  };
+
+  useEffect(() => {
+    const handleKeyPress = (e) => {
+      if (e.key === "Escape") {
+        showLogout && setShowLogout(false);
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyPress);
+    return () => {
+      document.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [setShowLogout, showLogout]);
+
+  useEffect(() => {
+    if (showLogout) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [showLogout]);
+
+  return (
+    <ModalGeneral fn={setShowLogout}>
+      <ImgContainer>
+        <div>
+          <img src={imgModal} alt="Animated white-orange cat" />
+        </div>
+      </ImgContainer>
+      <BtnsBox>
+        <LogoutConfirmBtn type="button" onClick={handleLogout}>
+          Yes
+        </LogoutConfirmBtn>
+        <CancelBtn type="button" onClick={handleCloseModal}>
+          No
+        </CancelBtn>
+      </BtnsBox>
+    </ModalGeneral>
+  );
+};
+export default ModalLogout;
---
+++ src/components/ForProfilePage/ModalLogout/ModalLogout.styled.jsx
@@ -0,0 +1,110 @@
+import styled from "styled-components";
+
+export const ImgContainer = styled.div`
+  width: calc(100dvw - 40px);
+  max-width: 295px;
+  text-align: center;
+
+  div {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+    background-color: var(--accent-light);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 auto;
+
+    img {
+      width: 44px;
+      height: 44px;
+    }
+  }
+
+  p {
+    font-weight: 700;
+    font-size: 20px;
+    letter-spacing: -0.03em;
+    color: var(--dark-color);
+    margin-top: 20px;
+  }
+
+  @media screen and (min-width: 768px) {
+    max-width: 288px;
+    height: 204px;
+    padding-top: 40px;
+    margin: 0 auto;
+
+    p {
+      font-size: 24px;
+      line-height: 1.17;
+    }
+  }
+`;
+
+export const BtnsBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 8px;
+  margin-top: 28px;
+
+  @media screen and (min-width: 768px) {
+    margin-top: 0px;
+    margin-bottom: 40px;
+  }
+`;
+
+export const LogoutConfirmBtn = styled.button`
+  outline: none;
+  border: none;
+  width: 137px;
+  height: 42px;
+  border-radius: 30px;
+  color: var(--white-color);
+  background-color: var(--accent-color);
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -0.03em;
+  transition: var(--hover-general);
+
+  &:hover {
+    background-color: var(--hover-color);
+    color: var(--accent-color);
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 140px;
+    height: 48px;
+    font-size: 16px;
+    line-height: 1.25;
+  }
+`;
+
+export const CancelBtn = styled.button`
+  outline: none;
+  border: none;
+  width: 137px;
+  height: 42px;
+  border-radius: 30px;
+  color: var(--dark-color);
+  background-color: var(--bg-button);
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -0.03em;
+  transition: var(--hover-general);
+
+  &:hover {
+    background-color: var(--hover-color);
+    color: var(--accent-color);
+  }
+
+  @media screen and (min-width: 768px) {
+    width: 140px;
+    height: 48px;
+    font-size: 16px;
+    line-height: 1.25;
+  }
+`;
