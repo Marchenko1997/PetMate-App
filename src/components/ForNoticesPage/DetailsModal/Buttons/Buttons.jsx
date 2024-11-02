c5a7f1e96c0449a71244e062203b1bbadc925ffd
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-02 22:48:26 +0100

    creating DetailsModal

--- src/components/ForNoticesPage/DetailsModal/Buttons/Buttons.jsx
+++ src/components/ForNoticesPage/DetailsModal/Buttons/Buttons.jsx
@@ -1,20 +1,52 @@
 // @ts-nocheck
-import React from "react";
+
 import { useDispatch } from "react-redux";
 import { useAuth } from "../../../../hooks/useAuth";
 import {
   AddToFavorites,
   RemoveFromFavorites,
 } from "../../../../redux/notices/operations";
-import sprite from "../../../../assets/icons/sprite.svg";
+import sprite from "../../../../assets/icons/icons.svg";
 import { AddRemoveBtn, ButtonsContainer, ContactBtn } from "./Buttons.styled";
 
+const Buttons = ({
+  isFavorite,
+  id,
+  setIsFavorite,
+  setShowFirstNotification,
+}) => {
+  const dispatch = useDispatch();
+  const { favoritesNotices } = useAuth();
 
+  const handleAddFavorites = () => {
+    if (favoritesNotices?.length === 0) {
+      setShowFirstNotification(true);
+    }
+    dispatch(AddToFavorites(id));
+    setIsFavorite(true);
+  };
 
-const Buttons = () => {
+  const handleRemoveFavorites = () => {
+    dispatch(RemoveFromFavorites(id));
+    setIsFavorite(false);
+  };
+
   return (
-    <div>Buttons</div>
-  )
-}
+    <ButtonsContainer>
+      <AddRemoveBtn
+        type="button"
+        onClick={isFavorite ? handleRemoveFavorites : handleAddFavorites}
+      >
+        {isFavorite ? "Remove from" : "Add to"}
+        <svg width={18} height={18}>
+          <use xlinkHref={`${sprite}#icon-heart`}></use>
+        </svg>
+      </AddRemoveBtn>
+      <ContactBtn to={"mailto:biggest5family@gmail.com"} target="_blank">
+        Contact
+      </ContactBtn>
+    </ButtonsContainer>
+  );
+};
 
-export default Buttons
+export default Buttons;
--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,21 +1,30 @@
 import { useSelector } from "react-redux";
 import {
-    selectUser,
-    selectIsLoggedIn,
-    selectIsLoading,
-    selectIsRefreshing
-} from "../redux/auth/selectors.js"
+  selectUser,
+  selectIsLoggedIn,
+  selectIsRefreshing,
+  selectIsLoading,
+  selectFavoritesNotices,
+  selectViewedNotices,
+  selectPets,
+} from "../redux/auth/selectors";
 
 export const useAuth = () => {
-   const isLoggedIn = useSelector(selectIsLoggedIn);
-   const isRefreshing = useSelector(selectIsRefreshing);
-   const isLoading = useSelector(selectIsLoading);
-   const user = useSelector(selectUser);
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const isLoading = useSelector(selectIsLoading);
+  const user = useSelector(selectUser);
+  const favoritesNotices = useSelector(selectFavoritesNotices);
+  const viewedNotices = useSelector(selectViewedNotices);
+  const pets = useSelector(selectPets);
 
-    return {
-        isLoggedIn,
-        isLoading,
-        isRefreshing,
-        user
-    }
-}
+  return {
+    isLoggedIn,
+    isRefreshing,
+    isLoading,
+    user,
+    favoritesNotices,
+    viewedNotices,
+    pets,
+  };
+};
