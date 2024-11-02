c70d4daef480b77371b3390db60ed4db890f31e2
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-02 21:56:37 +0100

    creating redux for notices

--- src/redux/notices/operations.js
+++ src/redux/notices/operations.js
@@ -3,3 +3,94 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = import.meta.env.REACT_APP_BASE_URL;
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+// GET CATEGORIES FOR SELECT
+export const fetchCategories = createAsyncThunk(
+  "categories/fetchAll",
+  async (thunkAPI) => {
+    try {
+      setAuthHeader();
+      const response = await axios.get("/notices/categories");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// GET GENDER TYPES FOR SELECT
+export const fetchGenders = createAsyncThunk(
+  "sex/fetchAll",
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get("/notices/sex");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// GET SPECIES TYPES FOR SELECT
+export const fetchSpecies = createAsyncThunk(
+  "species/fetchAll",
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get("/notices/species");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// GET CITIES LOCATIONS FOR SELECT
+export const fetchCities = createAsyncThunk(
+  "cities/fetchAll",
+  async (thunkAPI) => {
+    try {
+      const response = await axios.get("/cities");
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// ADD NOTICE TO FAVORITES
+export const AddToFavorites = createAsyncThunk(
+  "notices/addFavorites",
+  async (id, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    try {
+      setAuthHeader(persistedToken);
+      const response = await axios.post(`/notices/favorites/add/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+// REMOVE NOTICE FROM FAVORITES
+export const RemoveFromFavorites = createAsyncThunk(
+  "notices/removeFavorites",
+  async (id, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    try {
+      setAuthHeader(persistedToken);
+      const response = await axios.delete(`/notices/favorites/remove/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
