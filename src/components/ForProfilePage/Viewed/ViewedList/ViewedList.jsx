3affd4a5f6510540b8eb85c5801dc5564b3f43e7
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-03 11:58:57 +0100

    Updating ViewedList

---
+++ src/components/ForProfilePage/Viewed/ViewedList/ViewedList.jsx
@@ -0,0 +1,23 @@
+import  { useState } from "react";
+import { useAuth } from "../../../../hooks/useAuth";
+import Card from "../../../ForNoticesPage/Card/Card";
+import { ListFavorites } from "../../Favorites/Favorites.styled";
+
+const ViewedList = () => {
+  const { viewedNotices } = useAuth();
+  const [, setShowFirstNotification] = useState(false);
+  return (
+    <ListFavorites>
+      {viewedNotices.map((notice) => (
+        <Card
+          key={notice._id}
+          notice={notice}
+          setShowAttention={null}
+          setShowFirstNotification={setShowFirstNotification}
+        />
+      ))}
+    </ListFavorites>
+  );
+};
+
+export default ViewedList;
