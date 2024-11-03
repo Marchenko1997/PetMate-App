4c58026fcc033834585c18e5b41d9d665e42a060
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-03 14:05:29 +0100

    Updating Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,39 +1,47 @@
+// @ts-nocheck
 import { useState, useEffect } from "react";
-import { useMediaQuery } from 'react-responsive';
-import AuthNav from "../AuthNav/AuthNav";
-import NavMenu from "./NavMenu/NavMenu";
-import {useLocation} from 'react-router-dom';
-import { HeaderContainer, BlockBtns, MainHeader } from "./Header.styled";
+import { useMediaQuery } from "react-responsive";
+import { useLocation } from "react-router-dom";
 import UserProfileBtn from "../UserProfileBtn/UserProfileBtn";
+import { useAuth } from "../../hooks/useAuth";
 import Logo from "./Logo/Logo";
+import BurgerBtn from "./BurgerBtn/BurgerBtn";
+import LogoutHeader from "./LogoutHeader/LogoutHeader";
+import NavMenu from "./NavMenu/NavMenu";
+import AuthNav from "../AuthNav/AuthNav";
+import { BlockBtns, HeaderContainer, MainHeader } from "./Header.styled";
 
 const Header = () => {
- const tablet = useMediaQuery({ minWidth: 768 });
- const tabletEnd = useMediaQuery({ maxWidth: 1279.98 });
+  const tablet = useMediaQuery({ minWidth: 768 });
+  const tabletEnd = useMediaQuery({ maxWidth: 1279.98 });
   const desktop = useMediaQuery({ minWidth: 1280 });
   const location = useLocation();
   const [isHomepage, setIsHomepage] = useState(false);
-  
+  const { isLoggedIn } = useAuth();
+
   useEffect(() => {
     if (location.pathname === "/home" || location.pathname === "/") {
       setIsHomepage(true);
     } else {
       setIsHomepage(false);
     }
- }, [location.pathname]);
-
+  }, [location.pathname]);
 
   return (
-      <MainHeader>
-          <HeaderContainer isHomepage={isHomepage.toString()}>
+    <MainHeader>
+      <HeaderContainer ishomepage={isHomepage.toString()}>
         <Logo isHomepage={isHomepage} />
         {desktop && <NavMenu isHomepage={isHomepage} />}
-          <BlockBtns>
-          <AuthNav />
-          </BlockBtns >
-        </HeaderContainer>
+        <BlockBtns>
+          {((!isLoggedIn && !isHomepage && tablet && tabletEnd) ||
+            (!isLoggedIn && desktop)) && <AuthNav />}
+          {isLoggedIn && !isHomepage && tablet && <LogoutHeader />}
+          {isLoggedIn && <UserProfileBtn isHomepage={isHomepage} />}
+          <BurgerBtn isHomepage={isHomepage} />
+        </BlockBtns>
+      </HeaderContainer>
     </MainHeader>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
