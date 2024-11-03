e85bdceb78a2d4833197a0b67c3d9503cff0ad57
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-03 11:04:47 +0100

    creating petInfo

---
+++ src/components/ForProfilePage/PetsList/PetInfo/PetInfo.jsx
@@ -0,0 +1,29 @@
+
+import { formatBirthday } from "../../../../functions/formatBirthday";
+import { ListInfoPet } from "./PetInfo.styled";
+
+const PetInfo = ({ pet }) => {
+  const { name, birthday, sex, species } = pet;
+  const formattedDate = formatBirthday(birthday);
+
+  const details = [
+    { label: "Name", value: name },
+    { label: "Birthday", value: formattedDate },
+    { label: "Sex", value: sex },
+    { label: "Species", value: species },
+  ];
+
+  return (
+    <ListInfoPet>
+      {details.map(({ label, value }) => (
+        <li key={label}>
+          <p>
+            {label} <span>{value}</span>
+          </p>
+        </li>
+      ))}
+    </ListInfoPet>
+  );
+};
+
+export default PetInfo;
