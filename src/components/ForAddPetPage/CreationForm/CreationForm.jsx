bb27610e8e32d351ceef7326d86c4c808ba98ae5
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-04 19:32:11 +0100

    creating ImageDog

---
+++ src/components/ForAddPetPage/CreationForm/CreationForm.jsx
@@ -0,0 +1,140 @@
+// @ts-nocheck
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { addPet } from "../../../redux/auth/operations";
+import sprite from "../../../assets/icons/sprite.svg";
+import SexButtons from "./SexButtons/SexButtons";
+import PetAvatar from "./PetAvatar/PetAvatar";
+import { addPetSchema } from "../../../schemas/schemas";
+import BirthdayInput from "./BirthdayInput/BirthdayInput";
+import TypeAnimal from "./TypeAnimal/TypeAnimal";
+import {
+  BackBtn,
+  BoxContainer,
+  ButtonsBox,
+  ErrorMsg,
+  InputBox,
+  InputNormal,
+  InputPetImage,
+  InvisibleInput,
+  SubmitBtn,
+  UploadContainer,
+} from "./CreationForm.styled";
+import {
+  Label,
+  AddAvatarInput,
+} from "../../ForProfilePage/EditProfileModal/FormEditUser/FormEditUser.styled";
+
+const CreationForm = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const preset_key = import.meta.env.REACT_APP_PRESET_KEY;
+  const cloudURL = import.meta.env.REACT_APP_CLOUDINARY_URL;
+  const [sexPet, setSexPet] = useState("unknown");
+  const [petImageURL, setPetImageURL] = useState("");
+  const [petType, setPetType] = useState(null);
+  const [birthDate, setBirthDate] = useState(null);
+
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(addPetSchema),
+  });
+
+  const onSubmit = (data) => {
+    dispatch(addPet(data));
+    navigate("/profile");
+  };
+
+  useEffect(() => {
+    if (sexPet === "unknown") setValue("sex", setSexPet);
+
+    if (petType) setValue("species", petType);
+
+    if (birthDate) setValue("birthday", birthDate);
+  }, [birthDate, petType, sexPet, setValue]);
+
+  const handleUploadAvatar = (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", preset_key);
+    fetch(cloudURL, { method: "POST", body: formData })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Upload failed");
+        }
+        return res.json();
+      })
+      .then((data) => setValue("imgURL", data.secure_url))
+      .catch((error) => console.log("Upload error:", error.message));
+    const fileURL = URL.createObjectURL(file);
+    setPetImageURL(fileURL);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <SexButtons setSexPet={setSexPet} sexPet={sexPet} />
+      <InvisibleInput type="text" {...register("sex")} />
+      <ErrorMsg>{errors.sex?.message}</ErrorMsg>
+      <PetAvatar petImageUrl={petImageURL} />
+      <UploadContainer>
+        <label>
+          <InputPetImage
+            type="text"
+            {...register("imgURL")}
+            placeholder="Enter URL"
+          />
+          <ErrorMsg>{errors.imgURL?.message}</ErrorMsg>
+        </label>
+        <Label>
+          <AddAvatarInput
+            type="file"
+            name="avatarFile"
+            onChange={handleUploadAvatar}
+          />
+          <svg width={18} height={18}>
+            <use xlinkHref={`${sprite}#icon-upload-cloud`}></use>
+          </svg>
+        </Label>
+      </UploadContainer>
+      <InputBox>
+        <InputNormal type="text" {...register("title")} placeholder="Title" />
+        <ErrorMsg>{errors.title?.message}</ErrorMsg>
+      </InputBox>
+      <InputBox>
+        {" "}
+        <InputNormal
+          type="text"
+          {...register("name")}
+          placeholder="Pet's Name"
+        />
+        <ErrorMsg>{errors.name?.message}</ErrorMsg>
+      </InputBox>
+      <BoxContainer>
+        <div>
+          <BirthdayInput setBirthDate={setBirthDate} birthDate={birthDate} />
+          <ErrorMsg>{errors.birthday?.message}</ErrorMsg>
+          <InvisibleInput type="text" {...register("birthday")} />
+        </div>
+        <div>
+          <TypeAnimal setPetType={setPetType} petType={petType} />
+          <ErrorMsg>{errors.species?.message}</ErrorMsg>
+          <InvisibleInput type="text" {...register("species")} />
+        </div>
+      </BoxContainer>
+      <ButtonsBox>
+        <BackBtn to={"/profile"}>Back</BackBtn>
+        <SubmitBtn type="submit">Submit</SubmitBtn>
+      </ButtonsBox>
+    </form>
+  );
+};
+
+export default CreationForm;
---
+++ src/components/ForAddPetPage/ImageDog/ImageDog.styled.jsx
@@ -0,0 +1,61 @@
+import styled from "styled-components";
+
+export const ImageBox = styled.div`
+  position: relative;
+  background-color: var(--accent-color);
+  border-radius: 30px;
+  overflow: hidden;
+
+  picture {
+    position: relative;
+    z-index: 1;
+
+    img {
+      position: relative;
+      left: 6%;
+      animation: appearDown 1000ms ease 1;
+    }
+  }
+
+  svg {
+    position: absolute;
+    bottom: -16px;
+    left: 11px;
+    transform: scale(1.6);
+    z-index: 0;
+  }
+
+  @media screen and (min-width: 768px) {
+    border-radius: 60px;
+
+    picture {
+      img {
+        left: 30%;
+      }
+    }
+  }
+
+  svg {
+    transform: scale(2.8);
+    bottom: -82px;
+    left: 223px;
+  }
+
+  @media screen and (min-width: 1280px) {
+    width: 592px;
+    height: 654px;
+
+    picture {
+      right: unset;
+      left: -132px;
+      bottom: -8px;
+    }
+
+    svg {
+      transform: scale(4);
+      bottom: unset;
+      top: 278px;
+      left: 134px;
+    }
+  }
+`;
