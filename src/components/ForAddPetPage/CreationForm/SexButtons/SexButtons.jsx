2b4327ff7fb8dc8e9805f86403c80531fb7f78f8
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-04 12:16:28 +0100

    creating sex buttons

---
+++ src/components/ForAddPetPage/CreationForm/SexButtons/SexButtons.jsx
@@ -0,0 +1,41 @@
+// @ts-nocheck
+
+import sprite from "../../../../assets/icons/sprite.svg";
+import { FemaleLi, ListItems, MaleLi, MultipleLi } from "./SexButtons.styled";
+
+const SexButtons = ({ setSexPet, sexPet }) => {
+  const handleChooseSex = (e) => {
+    const choosenValue = e.currentTarget.dataset.value;
+    setSexPet(choosenValue);
+  };
+
+  return (
+    <ListItems>
+      <FemaleLi
+        data-value="female"
+        onClick={handleChooseSex}
+        datavalue={sexPet}
+      >
+        <svg width={24} height={24} datavalue={sexPet}>
+          <use xlinkHref={`${sprite}#icon-female`}></use>
+        </svg>
+      </FemaleLi>
+      <MaleLi data-value="male" onClick={handleChooseSex} datavalue={sexPet}>
+        <svg width={24} height={24} datavalue={sexPet}>
+          <use xlinkHref={`${sprite}#icon-male`}></use>
+        </svg>
+      </MaleLi>
+      <MultipleLi
+        data-value="multiple"
+        onClick={handleChooseSex}
+        datavalue={sexPet}
+      >
+        <svg width={24} height={24} datavalue={sexPet}>
+          <use xlinkHref={`${sprite}#icon-sex`}></use>
+        </svg>
+      </MultipleLi>
+    </ListItems>
+  );
+};
+
+export default SexButtons;
