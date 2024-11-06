066e89d890e7dba53603a5824a78ad3ebd616b2e
Halyna Marchenko <marchenkohalyna888@gmail.com>
2024-11-06 12:25:21 +0100

    creating redux for news

---
+++ src/redux/news/operations.js
@@ -0,0 +1,23 @@
+// @ts-nocheck
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
+
+// GET ALL NEWS FROM BACKEND BY PAGE AND/OR FILTER
+export const fetchNews = createAsyncThunk(
+  "news/fetchAll",
+  async ({ page, searchQuery }, thunkAPI) => {
+    let response;
+    try {
+      if (!searchQuery) {
+        response = await axios.get(`/news?page=${page}`);
+      } else {
+        response = await axios.get(`/news?page=${page}&keyword=${searchQuery}`);
+      }
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,12 +3,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "./auth/authSlice";
 import { NoticesReducer } from "./notices/noticeSlice";
 import { FriendsReducer } from "./friends/friendsSlice";
+import { NewsReducer } from "./news/newsSlice";
 
  const store = configureStore({
     reducer: {
          auth: authReducer,
          notices: NoticesReducer,
-         friends: FriendsReducer
+         friends: FriendsReducer,
+         news: NewsReducer
     } 
 
 })
